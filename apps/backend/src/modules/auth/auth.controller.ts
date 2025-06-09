69df4e89da88b68c98ff8b2ecd9e29697cff5a7a
manuel <c28390451@gmail.com>
2025-06-09 03:20:25 -0500

    Back-end:Auth:LogIn-LogOut >> se implementa el inicio de sesion y la finalizacion de sesion con creacion de token y refresco de token, se hace prueba unitaria de ambos metodos, funcionales correctamente

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Post, Body, BadRequestException, UnauthorizedException, Res } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException, UnauthorizedException, Res, Get, Query } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { User } from '../users/entities/user.entity';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginUserDto } from './dto/login.dto';
 import { ValidateUserDto } from './dto/validate.dto';
+import { UsersService } from '../users/users.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(
+        private readonly authService: AuthService,
+        private readonly usersService: UsersService,
+    ) {}
 
     @Post('register')
     async register(@Body() user: CreateUserDto):Promise<Partial<User>> {
@@ -38,12 +42,18 @@ export class AuthController {
             maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
         });
 
-        return { message: 'Logged In' };
+        return { access_token };
     }
 
     @Post('logout')
     async logout(@Res({ passthrough:true }) res: Response){
         res.clearCookie('access_token');
+        res.clearCookie('refresh_token');
         return { message: 'Logged out.' };
+    }
+
+    @Get('profile')
+    async profile(@Query('email') email:string){
+        return await this.usersService.findUserByEmail(email);
     }
 }
