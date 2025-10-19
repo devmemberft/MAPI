754f1c838d84f07d0fcc4a409c5e16ec1996a859
devmemberftexit <c28390451@gmail.com>
2025-10-19 21:46:27 +0200

    agregando script para crear las bases de datos primera vez montado el contenedor

--- init-db/init-multiple-dbs.sh
+++ init-db/init-multiple-dbs.sh
@@ -6,3 +6,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	CREATE DATABASE artecolConnection;
 	CREATE DATABASE accountingConnection;
 	CREATE DATABASE appointmentsConnection;
+EOSQL
